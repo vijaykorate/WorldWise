8d1c8fef81b3ed2f7f3c1103fa4f1b1eba1504ee
Vijay Korate <vijaykorate18@gmail.com>
2026-01-11 16:27:55 +0530

    creating worldwise

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
+
+function Homepage() {
+  return (
+    <div>
+      <PageNav />
+      <h1>WorldWise</h1>
+
+      <Link to="/app">Go to the app</Link>
+    </div>
+  );
+}
+
+export default Homepage;
